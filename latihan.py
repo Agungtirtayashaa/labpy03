27fe54aca7b074cbbfedd62b6811f5cc1cc1e9de
agung tirta yasha <agungtirta599@gmail.com>
2021-11-12 22:29:51 +0700

    menambahkan file latihan.py

---
+++ latihan.py
@@ -0,0 +1,12 @@
+print(" latihan1 ")
+
+a = input("Masukkan nilai N: ")
+
+for i in range(6):
+  print("data ke:=>",i)
+  from random import random 
+  a = random()
+  print(a)
+  while a <= 1:
+    print(a)
+    a += 1
