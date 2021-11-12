0f81e9bfd4947ba8ad9874ac3ec65ced119ac64e
agung tirta yasha <agungtirta599@gmail.com>
2021-11-13 01:08:52 +0700

    menambahkan file latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,9 @@
+rint(" latihan1 ")
+
+a = input("Masukkan nilai N: ")
+
+for i in range(6):
+  print("data ke:=>",i)
+  from random import random 
+  a = random()
+  print(a)
---
+++ program.py
@@ -0,0 +1,24 @@
+print('program1.py')
+
+a = 1000000
+
+for x in range(1,9):
+  if (x>=1 and x<=2):
+    b=a*0
+    print('laba bulanan ke-',x,':',b)
+
+  if (x>=3 and x<=4):
+    c=a*0.1
+    print('laba bulanan ke-',x,':',c)
+
+  if (x>=5 and x<=7) :
+    d=a*0.5
+    print('laba bulanan ke-',x,':',d)
+    
+
+  if (x==8) :
+    e=a*0.2
+    print('laba bulanan ke-',x,':',e)
+
+total = b+b+c+c+d+d+e
+print('\nTotal : ', total)
